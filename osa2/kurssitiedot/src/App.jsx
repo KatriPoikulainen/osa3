f21286647d1f28a33d2da1b047956b30f68485c8
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-02-24 17:33:50 +0200

    kurssitiedot step6

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,67 @@
+const Header = (props) => {
+  return<h1>{props.course.name}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part)=>(
+        <Part key={part.id} part={part}/>
+      ))}
+</div>
+  )
+}
+
+const Course = (props) => {
+  return(
+    <div>
+      <Header course={props.course}/>
+      <Content parts={props.course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Oma testi',
+        exercises: 26,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
